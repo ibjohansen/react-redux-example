3b4a59007544941fde5938b8dfbf7b3d933fbc5d
Ib Johansen <ib.johansen.jr@gmail.com>
2018-10-08 18:21:14 +0200

    added input and stuff

--- src/components/basic-react/index.jsx
+++ src/components/basic-react/index.jsx
@@ -1,11 +1,29 @@
 import React, {Component} from 'react'
 
 export class Basic extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {input: 'hallo på do'}
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(event){
+    const {value} = event.target
+    this.setState({input: value})
+  }
+
   render() {
-    const greeting = 'god kveld Abakus'
     return (
       <div>
-        {greeting}
+        <input
+          type="text"
+          value={this.state.input}
+          onChange={this.onChange}
+        />
+        <br/>
+        <p>{this.state.input}</p>
       </div>
     )
   }
